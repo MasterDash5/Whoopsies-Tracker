6c14f5d9d5d990f4998c1ab94380316f72bb7ef6
MasterDash5 <constant4337@molecularmail.com>
2025-11-19 18:31:51 -0700

    it works

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,33 +1,11 @@
 import express, { Request, Response } from "express"
 import * as dal from "./dal"
 
-interface Project {
-    id: string;
-    name: string;
-}
-
-interface Issue {
-    id: string;
-    title: string;
-    description: string;
-    date: Date;
-    commit: string;
-    projectId: string; // FK to Project
-}
-
-const test = async () => {
-    const projects = await dal.getProjects();
-    console.log(projects);
-};
-test();
-
 const app = express();
 const port: number = 3000;
 
-app.get("/projects", (req: Request, res: Response) => {
-    res.json([
-
-    ])
+app.get("/projects", async (req: Request, res: Response) => {
+    res.json(await dal.getProjects())
 });
 
 app.listen(port, () => {
