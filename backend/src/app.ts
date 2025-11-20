aa3fa4efc205d5cac3fdf8047be868d13535a888
MasterDash5 <constant4337@molecularmail.com>
2025-11-19 19:11:39 -0700

    project crud impl

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,8 +12,45 @@ app.get("/project/:id", async (req: Request, res: Response) => {
     res.json(await dal.getProject(req.params.id))
 });
 
-app.put("/project", async (req: Request, res: Response) => {
-    await dal.addProject(req.body.name);
+app.post("/project", async (req: Request, res: Response) => {
+    await dal.addProject({
+        id: 0, // ignored
+        name: req.body.name
+    });
+});
+
+app.get("/issues/:project_id", async (req: Request, res: Response) => {
+    res.json(await dal.getIssues(req.params.project_id));
+});
+
+app.get("/issue/:id", async (req: Request, res: Response) => {
+    res.json(await dal.getIssue(req.params.id));
+});
+
+app.post("/issue", async (req: Request, res: Response) => {
+    await dal.addIssue({
+        id: 0, // ignored
+        project_id: req.body.project_id,
+        created_at: 0, // ignored
+        resolved_at: null,
+        status: 0,
+        title: req.body.title,
+        description: req.body.description,
+        commit: req.body.commit
+    });
+});
+
+app.put("/issue/:id", async (req: Request, res: Response) => {
+    await dal.setIssue({
+        id: req.body.id,
+        project_id: req.body.project_id,
+        created_at: 0, // ignored
+        resolved_at: req.body.resolved_at,
+        status: req.body.status,
+        title: req.body.title,
+        description: req.body.description,
+        commit: req.body.commit
+    });
 });
 
 app.listen(port, () => {
