6a9c788b721d879ae472a3e23f7729f8de2c6312
MasterDash5 <constant4337@molecularmail.com>
2025-11-19 18:46:09 -0700

    pipe database and setup express (#4)
    * progress
    
    * it works

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import express, { Request, Response } from "express"
+import * as dal from "./dal"
+
+const app = express();
+const port: number = 3000;
+
+app.get("/projects", async (req: Request, res: Response) => {
+    res.json(await dal.getProjects())
+});
+
+app.listen(port, () => {
+    console.log("ExpressJS started");
+});
