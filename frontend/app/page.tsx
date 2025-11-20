a6c263fcd82756d9b758201b2f4c4d373fee9145
Hoochymoochy <Khaydenroberts22@gmail.com>
2025-11-19 17:59:22 -0700

    starter home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { useEffect } from "react";
 
 type Project = {
@@ -7,50 +6,48 @@ type Project = {
   name: string;
 };
 
-const mockdata = {
-  1: "Project 1",
-  2: "Project 2",
-  3: "Project 3",
-}
-
-
+const mockdata: Project[] = [
+  { id: 1, name: "Project 1" },
+  { id: 2, name: "Project 2" },
+  { id: 3, name: "Project 3" },
+  { id: 4, name: "Project 4" },
+  { id: 5, name: "Project 5" },
+  { id: 6, name: "Project 6" },
+];
 
 export default function Home() {
-  useEffect(() => {
-    
-  })
+  useEffect(() => {}, []);
+
   const projects: Project[] = [];
 
   return (
     <div className="flex min-h-screen justify-center">
-      <div className="flex flex-col items-center max-h-full w-1/3 bg-[#0a0a0a] relative">
-        <div className="bg-black w-full p-4">Projects tab</div>
 
-        <div className="flex flex-col items-center gap-5 mt-6">
-          {Object.entries(mockdata).map(([id, name]) => (
-            <button key={id} className="p-4 bg-black/30">
-              {name}
-            </button>
+      <div className="flex flex-col items-center w-96 relative border-r-2">
+        <div className="w-full p-4 text-center border-b-2">
+          Projects tab
+        </div>
+
+        <div className="flex flex-col overflow-y-scroll w-full">
+          {mockdata.map((project) => (
+            <div className="p-4 border-b-2" key={project.id}>
+              {project.name}
+            </div>
           ))}
         </div>
-        
-          <button className="p-4 rounded-full bg-white/10 absolute bottom-4">
-            Create Project
-          </button>
+
+        <button className="p-4 rounded-full absolute bottom-4 border-2 w-44">
+          Create Project
+        </button>
       </div>
 
-
-
-
-      <div className="flex flex-col items-center justify-center max-h-full w-2/3 dark-purple-bg">
-          {
-            projects && (
-              <div className="backdrop-blur-2xl bg-black p-4 border rounded-2xl border-black">
-                No projects Created
-              </div>
-            )
-          } 
-      </div> 
+      <div className="flex flex-col items-center justify-center w-full dark-purple-bg">
+        {projects.length === 0 && (
+          <div className="backdrop-blur-2xl p-4 border rounded-2xl">
+            No projects Created
+          </div>
+        )}
+      </div>
     </div>
   );
 }
