3c0add415997323557f606a6b1ee16503c2b54e0
MasterDash5 <constant4337@molecularmail.com>
2025-11-20 19:05:10 -0700

    project issue api (#6)

--- backend/src/dal.ts
+++ backend/src/dal.ts
@@ -1,9 +1,20 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 import { readFileSync } from 'fs';
 
-interface Project {
+export interface Project {
     id: number;
     name: string;
+}
+
+export interface Issue {
+    id: number;
+    project_id: number;
+    created_at: number;
+    resolved_at: number;
+    status: number;
+    title: string;
+    description: string;
+    commit: string;
 }
 
 const supabase_url: string = "https://mcotpbfsourpwfcsnddo.supabase.co";
@@ -18,7 +29,7 @@ export async function getProjects(): Promise<Project[]> {
     return data;
 }
 
-export async function getProject(id: string): Promise<Project> {
+export async function getProject(id: string): Promise<Project extends Id> {
     const { data, error } = await supabase.from('project').select('id,name').eq('id', id).single();
 
     if (error) throw error;
@@ -26,6 +37,42 @@ export async function getProject(id: string): Promise<Project> {
     return data;
 }
 
-export async function addProject(name: string): Promise<void> {
-    await supabase.from('project').insert({ name: name })
+export async function addProject(project: Project): Promise<void> {
+    delete project.id;
+    await supabase.from('project').insert(project);
+}
+
+export async function setProject(project: Project): Promise<void> {
+    await supabase.from('project').update(project);
+}
+
+export async function getIssues(project_id: string): Promise<Issue[]> {
+    const { data, error } = await supabase.from('issue')
+        .select('id,project_id,created_at,resolved_at,status,title,description,commit')
+        .eq('project_id', project_id);
+
+    if (error) throw error;
+
+    return data;
+}
+
+export async function getIssue(id: string): Promise<Issue> {
+    const { data, error } = await supabase.from('issue')
+        .select('id,project_id,created_at,resolved_at,status,title,description,commit')
+        .eq('id', id).single();
+    
+    if (error) throw error;
+
+    return data;
+}
+
+export async function addIssue(issue: Issue): Promise<void> {
+    delete issue.id;
+    delete issue.created_at;
+    await supabase.from('issue').insert(issue);
+}
+
+export async function setIssue(issue: Issue): Promise<void> {
+    delete issue.created_at;
+    await supabase.from('issue').update(issue);
 }
