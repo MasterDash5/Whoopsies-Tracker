57ea7b88c558e3dbf32e1f77f8198906e291bc0d
MasterDash5 <constant4337@molecularmail.com>
2025-11-24 19:16:12 -0700

    Add json body parser (#10)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express"
+import bodyParser from 'body-parser';
 import * as dal from "./dal"
 
 const app = express();
 const port: number = 3000;
+
+app.use(bodyParser.json());
 
 app.get("/projects", async (req: Request, res: Response) => {
     res.json(await dal.getProjects())
