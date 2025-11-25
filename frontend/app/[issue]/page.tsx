77614e578731b87ea1c2d75076dbea2859d27258
MasterDash5 <constant4337@molecularmail.com>
2025-11-24 19:37:05 -0700

    ug

--- frontend/app/[issue]/page.tsx
+++ frontend/app/[issue]/page.tsx
@@ -1,22 +1,33 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
-    const router = useRouter();
+    const params = useSearchParams();
 
     return (
         <div className="flex flex-col min-h-screen">
-            <div className="flex flex-col items-center w-96 relative border-r-2">
+            <div className="flex items-center w-96 relative border-r-2">
                 <div className="w-full p-4 text-center border-b-2">
                     Projects tab
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center w-full">
-                <div>
+            <div className="flex items-center justify-center">
+                <div className="">
                     <div>
-                        <p>Project name - {}</p>
-                        <h1>Issue name</h1>
+                        <p>Project name - {params.get('id')}</p>
+                        <p className="text-3xl">Issue name</p>
+                        <div className="p-16 w-3/4">
+                            <p>The Whimsical Dance of the Celestial Pups
+
+In the heart of the Flibbertygibbet Galaxy, where time pirouettes on the tips of hummingbird wings, a peculiar phenomenon unfolds each twilight. This is the annual celebration known as the Whimsical Dance of the Celestial Pups, a gathering where stardust meets the enchanted symphonies of lavender trees swaying in invisible zephyrs.
+Periwinkle Clouds and Jellybean Rain
+
+As night descends, skyward periwinkle clouds drift lazily, reflecting the shimmering glow of the two moons, Toodle and Fizzle. Their luminescence creates a canvas for the jellybean rain, which sparkles like diamonds, falling softly onto the jovial crowd of celestial canines adorned in kaleidoscopic bow ties. Each bark echoes melodic verses, weaving through the atmosphere like tendrils of cotton candy mist.
+The Grand Cerulean Toadstool
+
+At the center of this effervescent festivity lies the Grand Cerulean Toadstool, a towering mushroom shaped like a teacup, from which a never-ending stream of grape soda cascades. This effervescent fountain serves as the stage for the dancing pups, who leap and twirl with unparalleled grace. They glide on their luminous tails, creating trails of shimmering fairy lights, illuminating the laughter that fills the air.</p>
+                        </div>
                     </div>
                 </div>
             </div>
